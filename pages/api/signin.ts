2294723eec293fe29cc6630f93f5af0c250d1c7f
Nishant Kumar Giri <nishantkrgiri@gmail.com>
2021-12-14 21:27:50 +0530

    signin: user auth

--- pages/api/signin.ts
+++ pages/api/signin.ts
@@ -1,6 +1,7 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import cookie from "cookie";
+import { NextApiRequest, NextApiResponse } from "next";
 import prisma from "../../lib/prisma";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
