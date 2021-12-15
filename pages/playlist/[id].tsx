84fdb1c6fe4ab32b786c08f44e083a62ed8794e1
Nishant Kumar Giri <nishantkrgiri@gmail.com>
2021-12-16 03:59:51 +0530

    Playlist: gradient added

--- pages/playlist/[id].tsx
+++ pages/playlist/[id].tsx
@@ -1,8 +1,36 @@
+import GradientLayout from "../../components/gradientLayout";
 import { validateToken } from "../../lib/auth";
 import prisma from "../../lib/prisma";
 
+const getBGColor = (id) => {
+  const colors = [
+    "red",
+    "green",
+    "blue",
+    "purple",
+    "orange",
+    "yellow",
+    "gray",
+    "teal",
+  ];
+
+  return colors[id - 1] || colors[Math.floor(Math.random() * colors.length)];
+};
+
 const PlayList = ({ playlist }) => {
-  return <div>{playlist.name}</div>;
+  const color = getBGColor(playlist.id);
+  return (
+    <GradientLayout
+      color={color}
+      roundImage={false}
+      title={playlist.name}
+      subtitle="playlist"
+      description={`${playlist.songs.length} songs`}
+      image={`https://picsum.photos/400?random=${playlist.id}`}
+    >
+      <div>hello</div>
+    </GradientLayout>
+  );
 };
 
 export const getServerSideProps = async ({ query, req }) => {
