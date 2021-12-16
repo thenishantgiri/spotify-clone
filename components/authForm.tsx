eb217df66f7d84611b06024caf44c7bc3bb49b98
Nishant Kumar Giri <nishantkrgiri@gmail.com>
2021-12-16 17:34:35 +0530

    Fix: auth server side

--- components/authForm.tsx
+++ components/authForm.tsx
@@ -28,7 +28,7 @@ const AuthForm: FC<{ mode: "signin" | "signup" }> = ({ mode }) => {
         height="100px"
         borderBottom="white 1px solid"
       >
-        <NextImage src="/logo.svg" height={60} width={120} />
+        <NextImage src="/spotify.svg" height={60} width={120} />
       </Flex>
       <Flex justify="center" align="center" height="calc(100vh - 100px)">
         <Box padding="50px" bg="gray.900" borderRadius="6px">
--- pages/playlist/[id].tsx
+++ pages/playlist/[id].tsx
@@ -35,11 +35,22 @@ const PlayList = ({ playlist }) => {
 };
 
 export const getServerSideProps = async ({ query, req }) => {
-  const { id } = validateToken(req.cookies.TRAX_ACCESS_TOKEN);
+  let user;
+  try {
+    user = validateToken(req.cookies.TRAX_ACCESS_TOKEN);
+  } catch (e) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/signin",
+      },
+    };
+  }
+
   const [playlist] = await prisma.playlist.findMany({
     where: {
       id: +query.id,
-      userId: id,
+      userId: user.id,
     },
     include: {
       songs: {
