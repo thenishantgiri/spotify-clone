ff5bc25c10bf0ddde267b86b2c6c75e09a1c8892
Nishant Kumar Giri <nishantkrgiri@gmail.com>
2021-12-16 19:35:51 +0530

    PlayerBar

---
+++ components/playerBar.tsx
@@ -0,0 +1,17 @@
+import { Box, Flex, Text } from "@chakra-ui/layout";
+
+const PlayerBar = () => {
+  return (
+    <Box height="100px" width="100vw" bg="gray.900" padding="10px">
+      <Flex align="center">
+        <Box padding="20px" color="white" width="30%">
+          <Text fontSize="large">Song Name</Text>
+          <Text fontSize="sm">Artist Name</Text>
+        </Box>
+        <Box width="40%">Music Controls</Box>
+      </Flex>
+    </Box>
+  );
+};
+
+export default PlayerBar;
