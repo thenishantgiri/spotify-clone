165b30167a4750499be8e16445d51fcfd9fac7b5
Nishant Kumar Giri <nishantkrgiri@gmail.com>
2021-12-16 22:53:30 +0530

    Player: song state

--- components/playerBar.tsx
+++ components/playerBar.tsx
@@ -1,17 +1,26 @@
 import { Box, Flex, Text } from "@chakra-ui/layout";
+import { useStoreState } from "easy-peasy";
 import Player from "./player";
 
 const PlayerBar = () => {
+  const songs = useStoreState((state: any) => state.activeSongs);
+  const activeSong = useStoreState((state: any) => state.activeSong);
+
   return (
     <Box height="100px" width="100vw" bg="gray.900" padding="10px">
       <Flex align="center">
-        <Box padding="20px" color="white" width="30%">
-          <Text fontSize="large">Song Name</Text>
-          <Text fontSize="sm">Artist Name</Text>
-        </Box>
-        <Box width="40%">
-          <Player />
-        </Box>
+        {activeSong ? (
+          <Box padding="20px" color="white" width="30%">
+            <Text fontSize="large">{activeSong.name}</Text>
+            <Text fontSize="sm">{activeSong.artist.name}</Text>
+          </Box>
+        ) : null}
+
+        {activeSong ? (
+          <Box width="40%">
+            <Player songs={songs} activeSong={activeSong} />
+          </Box>
+        ) : null}
       </Flex>
     </Box>
   );
