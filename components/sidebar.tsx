bf16c0f1099270a4c97d11762f7cc4e4f37d48a0
thenishantgiri <nishantkrgiri@gmail.com>
2023-09-22 23:51:20 +0530

    sidebar: creating sidebar

---
+++ components/sidebar.tsx
@@ -0,0 +1,23 @@
+import {
+  Box,
+  List,
+  ListItem,
+  ListIcon,
+  Divider,
+  Center,
+  LinkBox,
+  LinkOverlay,
+} from "@chakra-ui/layout";
+import {
+  MdHome,
+  MdSearch,
+  MdLibraryMusic,
+  MdPlaylistAdd,
+  MdFavorite,
+} from "react-icons/md";
+
+const Sidebar = () => {
+  return <div>Hello from sidebar</div>;
+};
+
+export default Sidebar;
