e22ae0d5ffcd87aae99ad1f48144dc188c8c6317
thenishantgiri <nishantkrgiri@gmail.com>
2023-09-23 05:32:33 +0530

    auth: signin route created

---
+++ pages/api/signin.ts
@@ -0,0 +1,54 @@
+// Import necessary libraries and modules
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import cookie from "cookie";
+import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../../lib/prisma"; // Import your Prisma client instance
+
+// Define an asynchronous API endpoint handler function
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  // Destructure email and password from the request body
+  const { email, password } = req.body;
+
+  // Try to find a user with the provided email using Prisma
+  const user = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  // Check if the user exists and if the provided password matches the hashed password
+  if (user && bcrypt.compareSync(password, user.password)) {
+    // Generate a JWT token for authentication
+    const token = jwt.sign(
+      {
+        id: user.id, // User's ID
+        email: user.email, // User's email
+        time: Date.now(), // Current timestamp
+      },
+      "hello_secret", // Secret key used for signing the token
+      {
+        expiresIn: "8h", // Token expiration time (8 hours)
+      }
+    );
+
+    // Set a secure HTTP-only cookie containing the JWT token
+    res.setHeader(
+      "Set-Cookie",
+      cookie.serialize("TRAX_ACCESS_TOKEN", token, {
+        httpOnly: true, // Accessible only through HTTP
+        maxAge: 8 * 60 * 60, // Max age of the cookie (8 hours)
+        path: "/", // Path where the cookie is valid
+        sameSite: "lax", // Same-site cookie attribute
+        secure: process.env.NODE_ENV === "production", // Cookie is secure in production environment
+      })
+    );
+
+    // Send a JSON response containing the authenticated user data
+    res.json(user);
+  } else {
+    // If authentication fails, send a 401 Unauthorized response
+    res.status(401);
+    res.json({ error: "Email or Password is wrong" });
+  }
+};
