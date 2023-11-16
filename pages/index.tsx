9c08fa56ddddba54f805eb40a95662f5c898ac6f
thenishantgiri <nishantkrgiri@gmail.com>
2023-11-16 14:16:54 +0545

    UI Layout: Index - Load Artists Data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,27 @@
+import GradientLayout from "../components/gradientLayout";
+import prisma from "../lib/prisma";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <GradientLayout
+      color="green"
+      image="https://avatars.githubusercontent.com/u/56505350?v=4"
+      title="Nishant Giri"
+      subtitle="profile"
+      description="6 Public Playlists"
+      roundImage
+    >
+      <div>Home</div>
+    </GradientLayout>
+  );
+};
+
+export const getServerSideProps = async () => {
+  const artists = await prisma.artist.findMany({});
+
+  return {
+    props: { artists },
+  };
 };
 
 export default Home;
