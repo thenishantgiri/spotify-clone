27df00cfc6480b469d882d2d2a528d0e3e027e5e
thenishantgiri <nishantkrgiri@gmail.com>
2023-11-17 10:58:05 +0545

    Dynamic UI: build dynamic playlist pages

---
+++ pages/playlist/[id].tsx
@@ -0,0 +1,36 @@
+import { validateToken } from "../../lib/auth";
+import prisma from "../../lib/prisma";
+
+const Playlist = ({ playlist }) => {
+  return <div>{playlist.name}</div>;
+};
+
+export const getServerSideProps = async ({ query, req }) => {
+  const { id }: any = validateToken(req.cookies.TRAX_ACCESS_TOKEN);
+  const [playlist] = await prisma.playlist.findMany({
+    where: {
+      id: +query.id,
+      userId: id,
+    },
+    include: {
+      songs: {
+        include: {
+          artist: {
+            select: {
+              name: true,
+              id: true,
+            },
+          },
+        },
+      },
+    },
+  });
+
+  return {
+    props: {
+      playlist,
+    },
+  };
+};
+
+export default Playlist;
