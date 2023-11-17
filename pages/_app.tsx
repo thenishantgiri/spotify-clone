692163145a87a8e47695db0f77156e62f1d7ca4d
thenishantgiri <nishantkrgiri@gmail.com>
2023-11-17 15:42:44 +0545

    State Management: player state with Easy Peasy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,26 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import { StoreProvider } from "easy-peasy";
 import "reset-css";
 import theme from "../styles/theme.js";
 import PlayerLayout from "../components/playerLayout";
+import { store } from "../lib/store.js";
 
 const MyApp = ({ Component, pageProps }) => {
   return (
     // Wrap the entire application with the ChakraProvider using a specific theme
     <ChakraProvider theme={theme}>
-      {/* Conditional rendering based on the 'authPage' property of 'Component' */}
-      {Component.authPage ? (
-        // If 'authPage' is true, render 'Component' directly with 'pageProps'
-        <Component {...pageProps} />
-      ) : (
-        // If 'authPage' is false, wrap 'Component' with the 'PlayerLayout' component
-        <PlayerLayout>
+      <StoreProvider store={store}>
+        {/* Conditional rendering based on the 'authPage' property of 'Component' */}
+        {Component.authPage ? (
+          // If 'authPage' is true, render 'Component' directly with 'pageProps'
           <Component {...pageProps} />
-        </PlayerLayout>
-      )}
+        ) : (
+          // If 'authPage' is false, wrap 'Component' with the 'PlayerLayout' component
+          <PlayerLayout>
+            <Component {...pageProps} />
+          </PlayerLayout>
+        )}
+      </StoreProvider>
     </ChakraProvider>
   );
 };
