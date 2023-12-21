a3c8fe2df19f49aa6f830fb83b0a64a515c9fd02
thenishantgiri <nishantkrgiri@gmail.com>
2023-12-21 10:45:44 +0530

    Callbacks: End Song & Repeat Handlers

--- components/player.tsx
+++ components/player.tsx
@@ -25,13 +25,17 @@ import { formatTime } from "../lib/formatters";
 
 const Player = ({ songs, activeSong }) => {
   const [playing, setPlaying] = useState(true);
-  const [index, setIndex] = useState(0);
+  const [index, setIndex] = useState(
+    songs.findIndex((s) => s.id === activeSong.id)
+  );
   const [seek, setSeek] = useState(0.0);
   const [isSeeking, setIsSeeking] = useState(false);
   const [repeat, setRepeat] = useState(false);
   const [shuffle, setShuffle] = useState(false);
   const [duration, setDuration] = useState(0.0);
   const soundRef = useRef(null);
+  const repeatRef = useRef(repeat);
+  const setActiveSong = useStoreActions((state: any) => state.changeActiveSong);
 
   useEffect(() => {
     let timerId;
@@ -43,11 +47,19 @@ const Player = ({ songs, activeSong }) => {
       };
 
       timerId = requestAnimationFrame(f);
-      return () => cancelAnimationFrame(f);
+      return () => cancelAnimationFrame(timerId);
     }
 
     cancelAnimationFrame(timerId);
   }, [playing, isSeeking]);
+
+  useEffect(() => {
+    setActiveSong(songs[index]);
+  }, [index, setActiveSong, songs]);
+
+  useEffect(() => {
+    repeatRef.current = repeat;
+  }, [repeat]);
 
   const setPlayState = (value) => {
     setPlaying(value);
@@ -82,7 +94,7 @@ const Player = ({ songs, activeSong }) => {
   };
 
   const onEnd = () => {
-    if (repeat) {
+    if (repeatRef.current) {
       setSeek(0); // for seek ui
       soundRef.current.seek(0); // for seeking actual song
     } else {
